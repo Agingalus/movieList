e236e5add7ce381a460e2cbdb7d2589e0a4264e0
Caryswebsite1 <46387153+Caryswebsite1@users.noreply.github.com>
2019-04-19 14:00:14 -0700

    carys changes: modified to String and Valid
    Changed toString to produce colored stars for rating
    Changed Valid to check to make sure rating is an integer
    Changed initial rating value to convert input to a number if possible.

--- movie.js
+++ movie.js
@@ -1,13 +1,40 @@
 function Movie(pMovie, pRating) {
     this.movie = pMovie;
-    this.rating = pRating;
+    this.rating = Number(pRating);
 
-    this.toString = function() {
-        return this.movie + "      " + this.rating;
-    };
+    this.toString = function () {
+ 
+        let movieTitleString = this.movie + "  &nbsp; &nbsp;";
+        let movieRatingString = "";
 
-    this.valid = function() {
-        if (this.movie != "" && (this.rating > 0 && this.rating < 6)) {
+        //set color based on if it is good or not
+        //strong = gold, em = red
+        if (this.rating > 2) {
+            movieRatingString += "<strong>";
+        }
+        else {
+            movieRatingString += "<em>";
+        }
+
+        let i = 1;
+        for (i = 1; i <= this.rating; i++) {
+            movieRatingString += "&#9733";  // add a star!
+        }
+
+        if (this.rating > 2) {
+            movieRatingString += "</strong>";
+        }
+        else {
+            movieRatingString += "</em>";
+        }
+
+        return (movieTitleString + movieRatingString);
+
+    }; // end toString
+
+
+    this.valid = function () {
+        if (this.movie != "" && (Number.isInteger(this.rating) && this.rating > 0 && this.rating < 6)) {
             return true;
         } else {
             return false;
