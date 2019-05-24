37919b9e00468d5e855bb51138bc329af2819d3d
Aaron Greene <43617847+Agingalus@users.noreply.github.com>
2019-05-24 00:27:02 -0700

    added starting file for jq mobile

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
             4, { "SwitchCase": 1 }
         ],
         "linebreak-style": [
-            "error",
+            "warn",
             "unix"
         ],
         "quotes": [
