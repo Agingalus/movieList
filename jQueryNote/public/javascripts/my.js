a6c4d0a88f29ef2a1348275c0f7d463d60700636
Caryswebsite1 <46387153+Caryswebsite1@users.noreply.github.com>
2019-05-24 21:16:38 -0700

    fixed delete.

--- jQueryNote/public/javascripts/my.js
+++ jQueryNote/public/javascripts/my.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-undef */
 
-$(document).on("pagebeforeshow ", "#home", function() {
+$(document).on("pagebeforeshow ", "#home", function () {
     var info_view = ""; //string to put HTML in
     $("#noteDisplay").empty(); // since I do this everytime the page is redone, I need to remove existing before apending them all again
     $.getJSON("./noteList/") //Send an AJAX request
-        .done(function(data) {
-            $.each(data, function(index, record) { // make up each li as an <a> to the details-page
+        .done(function (data) {
+            $.each(data, function (index, record) { // make up each li as an <a> to the details-page
                 $("#noteDisplay").append("<li><a data-parm='" + record.Subject + "'  href='#details-page'>" + record.Subject + "</a></li>");
             });
             // $.each(data, function(index, record) { // make up each li as an <a> to the details-page
@@ -16,7 +16,7 @@ $(document).on("pagebeforeshow ", "#home", function() {
 
             $("#noteDisplay").listview("refresh"); // need this so jquery mobile will apply the styling to the newly added li's
 
-            $("a").on("click", function(event) { // set up an event, if user clicks any, it writes that items data-parm into the details page's html so I can get it there
+            $("a").on("click", function (event) { // set up an event, if user clicks any, it writes that items data-parm into the details page's html so I can get it there
                 var parm = $(this).attr("data-parm");
                 //do something here with parameter on  details page
                 $("#detailParmHere").html(parm);
@@ -28,17 +28,17 @@ $(document).on("pagebeforeshow ", "#home", function() {
 });
 
 
-$(document).on("pagebeforeshow", "#details-page", function() {
+$(document).on("pagebeforeshow", "#details-page", function () {
 
     var textString = "fix me";
     var id = $("#detailParmHere").text();
     $.getJSON("/findNote/" + id)
-        .done(function(data) {
-            textString = "Priority: " + data.Priority + "<br> Subject: " + data.Subject + "<br>Description: " + data.Description;
+        .done(function (data) {
+            textString = "Priority: " + data.Priority + "<br> Subject: " + data.Subject + "<br> Description: " + data.Description;
             $("#showdata").html(textString);
 
         })
-        .fail(function(jqXHR, textStatus, err) {
+        .fail(function (jqXHR, textStatus, err) {
             textString = "Sorry! Could not find it :(";
             $("#showdata").text(textString);
         });
@@ -47,7 +47,7 @@ $(document).on("pagebeforeshow", "#details-page", function() {
 
 });
 
-$(document).on("pagebeforeshow", "#deletePage", function() {
+$(document).on("pagebeforeshow", "#deletePage", function () {
 
     $("#deleteSubjectName").val("");
 });
@@ -55,20 +55,20 @@ $(document).on("pagebeforeshow", "#deletePage", function() {
 function deleteNote() {
     var note = $("#deleteSubjectName").val();
     $.ajax({
-        url: "/deleteProduct/" + note,
+        url: "/deleteNote/" + note,
         type: "DELETE",
         contentType: "application/json",
-        success: function(response) {
+        success: function (response) {
             alert("The note successfully deleted in cloud");
         },
-        error: function(response) {
-            alert("Product NOT deleted in cloud");
+        error: function (response) {
+            alert("ERROR: Note NOT deleted in cloud");
         }
     });
 }
 
 // clears the fields
-$(document).on("pagebeforeshow", "#addPage", function() {
+$(document).on("pagebeforeshow", "#addPage", function () {
     $("#newPriority").val("");
     $("#newSubject").val("");
     $("#newDescription").val("");
